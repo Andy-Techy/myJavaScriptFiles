4c9a96a6ef57997d4d596f228570248b784f8316
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2024-11-29 09:14:11 +0000

    tutorials/varibales.js

---
+++ tutorials/variables.js
@@ -0,0 +1,67 @@
+let name = "Andy" ;
+console.log (name);
+
+// declaring variable names
+
+// 1. cannot ba a reserve word
+// 2. should be meaningful and descriptive
+// 3. cannot start with a number
+// 4. cannot contain a space or hyphen
+// 5. are case-sensitive
+
+
+// constants
+let interestRate = 0.3;
+interestRate = 1;
+console.log (interestRate);
+
+
+let me = 'Andy'; // string 
+let age = 20; // number
+let isApproved = false; // boolean 
+let firstName = undefined;
+let lastName = null;
+
+
+
+
+//just refreshing my brains on javascript
+
+// let name = "mitch";
+// console.log(name);
+
+// console.log('andy');
+
+// food = prompt("enter your favorite food");
+
+// let price =   10;
+// console.log(price);
+// let quantity = 10;
+
+// let totalPrice = quantity * price;
+// console.log(totalPrice);
+
+// console.log("Your total price is " + "Ghs" + totalPrice + ".");
+
+
+
+
+/*math methods
+math.floor - rounds down
+math.ceil  - rounds up 
+math.random -
+
+*/
+
+// console.log(Math.floor(125.4875));
+// console.log(Math.ceil(125.4875));
+
+// console.log(Math.random());
+
+// let weather = 'sunny';
+
+// if (weather == 'rainy') {
+//     console.log('Grab your umbrella')
+// }else{
+//     console.log('Put on your sunglasses')
+// };
