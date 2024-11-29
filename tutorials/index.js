b81992bc72fc0444d239083c82ba580ea50ccaab
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2024-11-29 08:55:43 +0000

    firt js program

---
+++ tutorials/index.js
@@ -0,0 +1,124 @@
+// this is my first js code !
+console.log ('Hello World');
+
+// let name = "Andy" ;
+// console.log (name);
+
+// // declaring variable names
+
+// // 1. cannot ba a reserve word
+// // 2. should be meaningful and descriptive
+// // 3. cannot start with a number
+// // 4. cannot contain a space or hyphen
+// // 5. are case-sensitive
+
+
+// // constants
+// let interestRate = 0.3;
+// interestRate = 1;
+// console.log (interestRate);
+
+
+// let me = 'Andy'; // string 
+// let age = 20; // number
+// let isApproved = false; // boolean 
+// let firstName = undefined;
+// let lastName = null;
+
+
+// //objects
+// // let Name = 'Andy';
+// // let Age = 21;
+
+// let person = {
+//     Name : 'Andy',
+//     Age : 21
+// };
+
+// console.log (person)
+
+// // arrays
+
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 'green';
+// console.log(selectedColors);
+// console.log(selectedColors.length);
+
+
+// //functions
+// function greet(name, lastName) {
+//     console.log ('hello ' + name + ' ' + lastName);
+// }
+
+// greet('Andy', 'Michael');
+
+// //BASIC VARIABLE ASSIGNMENT
+
+// console.log("************************************");
+
+// var petDog = 'Rex';
+// var petCat = 'Pepper';
+// console.log(petDog);
+// console.log(petCat);
+
+// console.log("**************************************");
+// console.log ("My pet dog's name is: ", petDog);
+// console.log ("My pet cat's name is: ", petCat);
+
+// var catSound = "purr";
+// var dogSound = "woof";
+// console.log(petDog, " says ", dogSound);
+// console.log(petCat, " says ", catSound);
+
+// catSound = "meow";
+// console.log (petCat, " now says ", catSound);
+
+// console.log ("********************************************")
+// console.log ("********************************************")
+
+// //ADVANCE USE OF LOGICAL OPERATORS
+// // console.log ("********************************************")
+
+// // using the AND (&&) operator.
+
+// // you are coding an RPG game, where each character has certain skill levels based on the value saved in their score.
+// // 1. create a variable named score and set it to 8.
+// // 2. use console.log() that includes the string 'Mid-level skills:' and compares the 
+// //  score variable to above 0 and below 10 using the && operator.
+
+// var score = 8;
+
+// var skill = score > 0 &&  score < 10;
+// console.log("Mid-level skills: ", skill);
+
+// console.log ("********************************************")
+// console.log ("********************************************")
+
+
+
+// // using the OR (||) operator
+
+// // imagine you are coding a video game. Currently, you’re about to code some snippets related to the game over condition.
+
+// // you need to code a new variable named timeRemaining and set it to 0. You also need to code a new variable named energy and set it to 10.
+
+// // next, you should write a piece of code that could be used to determine if the game is over, based on whether either the value of the timeRemaining variable is 0 or the value of the energy variable is 0.
+
+// // complete the task using the following steps:
+
+// // declare the variable timeRemaining, and assign the value of 0 to it.
+
+// // declare the variable energy, and assign the value of 10 to it.
+
+// // console log the following parameters: "Game over: ", and timeRemaining == 0 || energy == 0
+
+
+// var timeRemaining = 0;
+// var energy = 10;
+// var gameOver = timeRemaining == 0 || energy == 0;
+// console.log("Game over:", gameOver);
+
+
+
+
+
